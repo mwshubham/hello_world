8f5034c9d26dec0033a0aa4c52894f057f49a061
Shubham Agarwal <mwshubham@gmail.com>
2020-08-22 07:56:22 +0530

    2. Create a simple Dart class

--- bin/Bicycle.dart
+++ bin/Bicycle.dart
@@ -1,7 +1,30 @@
 class Bicycle {
-  int candence;
-  int speed;
+  int cadence;
+
+  // private instance variable
+  int _speed = 2;
+
+  // getter
+  int get speed => _speed;
   int gear;
+
+  Bicycle(this.cadence, this.gear);
+
+  void applyBrakes(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
+
+  @override
+  String toString() {
+    return 'cadence: $cadence, speed: $speed, gear: $gear';
+  }
 }
 
-void main() {}
+void main(List<String> args) {
+  var bicycle = Bicycle(1, 3);
+  print(bicycle);
+}
